0aead6f7c70f4ec8df62fbb913786ab65b4c58d2
Prasad Kulkarni <ppk6585@gmail.com>
2016-12-06 01:23:58 +0530

    drop wizard metrics changes

--- src/main/java/com/ppk/controller/MicroAController.java
+++ src/main/java/com/ppk/controller/MicroAController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Timed;
 import com.ppk.entities.MSData;
-import com.ppk.service.MicroAServiceImpl;
+import com.ppk.service.MicroAService;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
@@ -16,13 +18,17 @@ import com.wordnik.swagger.annotations.ApiOperation;
 @RequestMapping
 public class MicroAController {
 
-	private final MicroAServiceImpl microAService;
+	private static final String		MS_BASE_METRIC_URL	= "/ms/v1/data";
+
+	private final MicroAService	microAService;
 
 	@Autowired
-	public MicroAController(MicroAServiceImpl microAService) {
+	public MicroAController(MicroAService microAService) {
 		this.microAService = microAService;
 	}
 
+	@Timed(name = MS_BASE_METRIC_URL + "/.GET", absolute = true)
+	@ExceptionMetered(name = MS_BASE_METRIC_URL + "/.GET." + ExceptionMetered.DEFAULT_NAME_SUFFIX, absolute = true)
 	@ApiOperation(value = "Get micro a data ", notes = "Get micro a data ")
 	@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody MSData welcomeMessage() {
--- src/main/java/com/ppk/service/MicroAServiceImpl.java
+++ src/main/java/com/ppk/service/MicroAServiceImpl.java
@@ -2,11 +2,15 @@ package com.ppk.service;
 
 import org.springframework.stereotype.Service;
 
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Timed;
 import com.ppk.entities.MSData;
 
 @Service
 public class MicroAServiceImpl implements MicroAService {
 
+	@Timed
+	@ExceptionMetered
 	@Override
 	public MSData getData() {
 		return new MSData("1234", "Title served from Micro A!!", 10);
